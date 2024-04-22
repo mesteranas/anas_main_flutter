88db0866138c668f4d965747a4bc0cc339cd0afc
mesteranas <135442710+mesteranas@users.noreply.github.com>
2024-04-22 20:16:14 +0200

    22/04/2024

---
+++ anasmain/lib/app.dart
@@ -0,0 +1,7 @@
+class App{
+  static const name="anas main";
+  static const appName="anas_main_flutter";
+  static const appDirName="anasmain";
+  static const version=1.0;
+  static const description="";
+}
---
+++ anasmain/lib/main.dart
@@ -0,0 +1,51 @@
+import 'app.dart';
+import 'contectUs.dart';
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+void main() {
+  runApp(const test());
+}
+class test extends StatefulWidget{
+  const test({Key?key}):super(key:key);
+  @override
+  State<test> createState()=>_test();
+}
+class _test extends State<test>{
+
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      title: App.name,
+      themeMode: ThemeMode.system,
+      home:Builder(builder:(context) 
+    =>Scaffold(
+      appBar:AppBar(
+        title: const Text(App.name),), 
+        drawer: Drawer(
+          child:ListView(children: [
+          DrawerHeader(child: Text("navigation menu")),
+          ListTile(title: Text("contect us"),onTap: (){
+            Navigator.push(context, MaterialPageRoute(builder: (context)=>ContectUsDialog()));
+          },),
+          ListTile(title: Text("donate"),onTap: (){
+            launch("https://www.paypal.me/AMohammed231");
+          },),
+  ListTile(title: Text("visite project on github"),onTap: (){
+    launch("https://github.com/mesteranas/"+App.appName);
+  },),
+  ListTile(title: Text("about"),onTap: (){
+    showDialog(context: context, builder: (BuildContext context){
+      return AlertDialog(title: Text("about "+App.name),content:Center(child:Column(children: [
+        ListTile(title: Text("version: " + App.version.toString())),
+        ListTile(title:Text("developer: mesteranas")),
+        ListTile(title:Text("description:" + App.description))
+      ],) ,));
+    });
+  },)
+        ],) ,),
+        body:Container(alignment: Alignment.center
+        ,child: Column(children: [const  Text("this app is created by mesteranas")
+    ])),)));
+  }
+}
