4b7652dd5a1a25944094e95728fad447e228be22
mesteranas <135442710+mesteranas@users.noreply.github.com>
2024-07-15 11:02:17 +0300

    15/07/2024

--- anasmain/lib/app.dart
+++
@@ -1,7 +0,0 @@
-class App{
-  static const name="anas main";
-  static const appName="anas_main_flutter";
-  static const appDirName="anasmain";
-  static const version=1.0;
-  static const description="";
-}
